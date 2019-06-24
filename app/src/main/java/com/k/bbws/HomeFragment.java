342f536e275f9d5d9d0c1ea61bbe08b4212747f4
ridwan <ridwan.22693@gmail.com>
2019-06-24 12:27:05 +0700

    bug search

--- app/src/main/java/com/k/bbws/HomeFragment.java
+++ app/src/main/java/com/k/bbws/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.k.bbws;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
@@ -16,6 +20,17 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@org.jetbrains.annotations.NotNull Message message) {
+            switch (message.what) {
+                case 1:{
+                    webViewGoBack();
+                }break;
+            }
+        }
+    };
 
     SwipeRefreshLayout swipeRefreshLayout;
     WebView webView;
@@ -59,11 +74,28 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             }
         });
 
+        webView.setOnKeyListener(new View.OnKeyListener(){
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK
+                        && event.getAction() == MotionEvent.ACTION_UP
+                        && webView.canGoBack()) {
+                    handler.sendEmptyMessage(1);
+                    return true;
+                }
+
+                return false;
+            }
+
+        });
+
         webView.loadUrl(URL);
 
         return view;
     }
-
+    private void webViewGoBack(){
+        webView.goBack();
+    }
     @Override
     public void onRefresh() {
         webView.reload();
